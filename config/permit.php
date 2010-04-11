609c545ec654494ebd099cbb5115c60ad38304e6
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-04-11 05:22:48 -0400

    correcting sample config

--- config/permit.php
+++ config/permit.php
@@ -5,8 +5,8 @@ App::import('Component', 'PermitComponent');
 /*
 Permit::access(
 	array('controller' => 'posts', 'action' => array('add', 'edit', 'delete')),
-	array('auth' => array('group' => 'admin'),
-	array('redirect' => array('controller' => 'users', 'action' => 'login'))));
+	array('auth' => array('group' => 'admin')),
+	array('redirect' => array('controller' => 'users', 'action' => 'login')));
 */
 
 ?>
