b6bbafa087723a2237c73187b2d95ad94d65608b
Jose Diaz-Gonzalez <myaccounts@savant.be>
2014-01-12 03:26:36 -0500

    format

--- Controller/SanctionPermissionsController.php
+++ Controller/SanctionPermissionsController.php
@@ -4,51 +4,51 @@ App::uses('SanctionAppController', 'Sanction.Controller');
 
 class SanctionPermissionsController extends SanctionAppController {
 
-  public function autocomplete() {
-    $this->viewClass = 'JsonView';
+	public function autocomplete() {
+		$this->viewClass = 'JsonView';
 
-    $type = $this->request->query('type');
-    if (empty($type)) {
-      $type = 'controller';
-    }
+		$type = $this->request->query('type');
+		if (empty($type)) {
+			$type = 'controller';
+		}
 
-    if (!in_array($type, array('plugin', 'controller', 'action'))) {
-      return $this->set(array(
-        '_serialize' => array('message', 'status'),
-        'message' => sprintf('Invalid type \'%s\'', $type)
-        'status' => 400
-      ));
-    }
+		if (!in_array($type, array('plugin', 'controller', 'action'))) {
+			return $this->set(array(
+				'_serialize' => array('message', 'status'),
+				'message' => sprintf('Invalid type \'%s\'', $type)
+				'status' => 400
+			));
+		}
 
-    return $this->set(array(
-      'status' => 200,
-      Inflector::pluralize($type) => $this->fetch($type);
-    ))
-  }
+		return $this->set(array(
+			'status' => 200,
+			Inflector::pluralize($type) => $this->fetch($type);
+		))
+	}
 
-  protected function fetch($type) {
-    $plugin = $this->request->query('plugin');
-    $controller = $this->request->query('controller');
+	protected function fetch($type) {
+		$plugin = $this->request->query('plugin');
+		$controller = $this->request->query('controller');
 
-    if ($type === 'plugin') {
-      return array(CakePlugin::loaded());
-    } elseif ($type === 'controller') {
-      if ($plugin) {
-        // Scope request by plugin
-      } else {
-        // Get app controllers
-      }
-    } elseif ($type === 'action') {
-      if ($plugin && $controller) {
-        // Scope request by plugin and controller
-      } elseif ($controller) {
-        // Scope by app controller
-      } else {
-        return array();
-      }
-    }
+		if ($type === 'plugin') {
+			return array(CakePlugin::loaded());
+		} elseif ($type === 'controller') {
+			if ($plugin) {
+				// Scope request by plugin
+			} else {
+				// Get app controllers
+			}
+		} elseif ($type === 'action') {
+			if ($plugin && $controller) {
+				// Scope request by plugin and controller
+			} elseif ($controller) {
+				// Scope by app controller
+			} else {
+				return array();
+			}
+		}
 
-    return array();
-  }
+		return array();
+	}
 
 }
--- Model/SanctionPermission.php
+++ Model/SanctionPermission.php
@@ -5,74 +5,75 @@ App::uses('CakeLog', 'Log');
 
 class SanctionPermission extends SanctionAppModel {
 
-// Array of rules upon which we will control access
-// Array of rules by which the User's session must be defined by
-//  - If you restrict to a single model and don't use associated data, you can enter just the fieldname to match on in the Auth array
-//  - If you use associated models, you need to specify a Set::extract() path as the fieldname
-// Array of extra parameters, such as where to redirect, the flash message, etc.
+ /**
+	* Array of rules upon which we will control access
+	* Array of rules by which the User's session must be defined by
+	*  - If you restrict to a single model and don't use associated data, you can enter just the fieldname to match on in the Auth array
+	*  - If you use associated models, you need to specify a Set::extract() path as the fieldname
+	* Array of extra parameters, such as where to redirect, the flash message, etc.
+	*/
+	public function savePermission($route, $rules = array(), $redirect = array()) {
+		return $this->save(array(
+			'route' => json_encode($route),
+			'rules' => json_encode($rules),
+			'redirect' => json_encode($redirect),
+		));
+	}
 
-  public function savePermission($route, $rules = array(), $redirect = array()) {
-    return $this->save(array(
-      'route' => json_encode($route),
-      'rules' => json_encode($rules),
-      'redirect' => json_encode($redirect),
-    ));
-  }
+	public function retrievePermissions() {
+		$keys = array('route', 'rules', 'redirect');
+		$_permissions = $this->find('all');
+		$permissions = array();
+		foreach ($_permissions as $i => $permission) {
+			foreach ($keys as $key) {
+				$permissions[$i][$key] = json_decode(
+					$_permissions[$i][$this->alias][$key],
+					true
+				);
+			}
+		}
+		return $permissions;
+	}
 
-  public function retrievePermissions() {
-    $keys = array('route', 'rules', 'redirect');
-    $_permissions = $this->find('all');
-    $permissions = array();
-    foreach ($_permissions as $i => $permission) {
-      foreach ($keys as $key) {
-        $permissions[$i][$key] = json_decode(
-          $_permissions[$i][$this->alias][$key],
-          true
-        );
-      }
-    }
-    return $permissions;
-  }
+	public function retrieveCachedPermissions() {
+		$config = $this->_getCacheConfig();
 
-  public function retrieveCachedPermissions() {
-    $config = $this->_getCacheConfig();
+		$permissions = Cache::read('sanction.permissions', $config);
+		if (!$permissions) {
+			$stream = $this->_getLogConfig();
+			CakeLog::notice('Loading permissions from database', array($stream));
+			$permissions = $this->retrievePermissions();
+			Cache::write('sanction.permissions', $permissions, $config);
+		}
 
-    $permissions = Cache::read('sanction.permissions', $config);
-    if (!$permissions) {
-      $stream = $this->_getLogConfig();
-      CakeLog::notice('Loading permissions from database', array($stream));
-      $permissions = $this->retrievePermissions();
-      Cache::write('sanction.permissions', $permissions, $config);
-    }
+		return $permissions;
+	}
 
-    return $permissions;
-  }
+	public function connectPermissions() {
+		$permissions = $this->retrieveCachedPermissions();
+		foreach ($permissions as $permission) {
+			Permit::access(
+				$permission['route'],
+				$permission['rules'],
+				$permission['redirect']
+			);
+		}
+	}
 
-  public function connectPermissions() {
-    $permissions = $this->retrieveCachedPermissions();
-    foreach ($permissions as $permission) {
-      Permit::access(
-        $permission['route'],
-        $permission['rules'],
-        $permission['redirect']
-      );
-    }
-  }
+	protected function _getLogConfig() {
+		$config = 'default';
+		if (in_array('sanction', CakeLog::configured())) {
+			$config = 'sanction';
+		}
+		return $config;
+	}
 
-  protected function _getLogConfig() {
-    $config = 'default';
-    if (in_array('sanction', CakeLog::configured())) {
-      $config = 'sanction';
-    }
-    return $config;
-  }
-
-  protected function _getCacheConfig() {
-    $config = 'default';
-    if (in_array('sanction', Cache::configured())) {
-      $config = 'sanction';
-    }
-    return $config;
-  }
+	protected function _getCacheConfig() {
+		$config = 'default';
+		if (in_array('sanction', Cache::configured())) {
+			$config = 'sanction';
+		}
+		return $config;
+	}
 
 }
