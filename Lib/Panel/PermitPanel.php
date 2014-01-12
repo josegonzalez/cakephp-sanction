305cc2feb4fa6278ed9663a36dcffd75fe69676f
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2014-01-12 02:18:10 -0500

    Don't use static calls to get attributes

--- Lib/Panel/PermitPanel.php
+++ Lib/Panel/PermitPanel.php
@@ -6,13 +6,13 @@ class PermitPanel extends DebugPanel {
 	public $plugin = 'sanction';
 
 	public function beforeRender(Controller $controller) {
-		if (empty(Permit::$user)) {
-			Permit::$user = $controller->Toolbar->Session->read(Permit::$settings['path']);
+		if (empty($controller->Permit->user)) {
+			$controller->Permit->user = $controller->Toolbar->Session->read($controller->Permit->settings['path']);
 		}
 		return array(
-			'user' => Permit::$user,
-			'routes' => Permit::$routes,
-			'executed' => Permit::$executed
+			'user' => $controller->Permit->user,
+			'routes' => $controller->Permit->routes,
+			'executed' => $controller->Permit->executed,
 		);
 	}
 
